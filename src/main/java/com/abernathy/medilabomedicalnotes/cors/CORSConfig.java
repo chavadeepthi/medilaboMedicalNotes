1519c7148b25dffd2a945492c813213928ba6213
chavadeepthi <ragadeepthi.chava@gmail.com>
2025-11-01 20:46:26 -0400

    Including Docker Installation

--- src/main/java/com/abernathy/medilabomedicalnotes/cors/CORSConfig.java
+++ src/main/java/com/abernathy/medilabomedicalnotes/cors/CORSConfig.java
@@ -13,7 +13,7 @@ public class CORSConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080", "http://localhost:8082")
+                        .allowedOrigins("http://localhost:8080", "http://localhost:8082", "http://medilabogateway:8080")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*")
